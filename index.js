8913605fbc6226efa59db598e3297fd4dc0596f3
nkunic <natasa.kunic@gmail.com>
2022-05-25 13:01:39 +0200

    1 file modified

--- index.js
+++ index.js
@@ -17,27 +17,28 @@ var round = 0;
 var player1Score = 0;
 var player2Score = 0;
 
-function bacanjeKockice() {
-  /* Eliminacija skakanja konzole */
+function throwingDice() {
+
+  /* Eliminate console jumping */
   console.clear();
 
   /* Round count */
   round++;
 
   /* Dice */
-  // var k1 = Math.floor(Math.random() * 6);
-  // var k2 = Math.floor(Math.random() * 6);
-  var kockica1 = Math.ceil(Math.random() * 6);
-  var kockica2 = Math.ceil(Math.random() * 6);
+  // var dice1 = Math.floor(Math.random() * 6);
+  // var dice2 = Math.floor(Math.random() * 6);
+  var dice1 = Math.ceil(Math.random() * 6);
+  var dice2 = Math.ceil(Math.random() * 6);
 
   /* Player score */
-  player1Score += kockica1;
-  player2Score += kockica2;
+  player1Score += dice1;
+  player2Score += dice2;
 
   /* Dice trowed */
-  //console.log(kockica1, kockica2);
+  //console.log(dice1, dice2);
   console.log('*** ROUND ' + round + ' ***');
-  console.log(player1 + ':' + kockica1 + ' --- ' + kockica2 + ':' + player2);
+  console.log(player1 + ':' + dice1 + ' --- ' + dice2 + ':' + player2);
 
   /* Current score */
   console.log(
@@ -56,11 +57,11 @@ function bacanjeKockice() {
   if (round === 10) {
     winner();
   } else {
-    setTimeout(bacanjeKockice, 3000);
+    setTimeout(throwingDice, 3000);
   }
 }
 
-bacanjeKockice();
+throwingDice();
 
 function winner() {
   if (player1Score > player2Score) {
